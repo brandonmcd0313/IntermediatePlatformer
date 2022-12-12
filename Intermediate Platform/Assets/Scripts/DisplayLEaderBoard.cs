3d4423b5db675e959792e412c41d943cbc340c98
brandonmcd0313 <bamcd0313@gmail.com>
2022-12-12 16:18:53 -0500

    Finished Leaderboard Mechanics
    Note: need to implement quiting the game into the game... current impossible to leave!

--- Intermediate Platform/Assets/Scripts/DisplayLEaderBoard.cs
+++ Intermediate Platform/Assets/Scripts/DisplayLEaderBoard.cs
@@ -17,40 +17,66 @@ public class DisplayLEaderBoard : MonoBehaviour {
     [SerializeField] Text smallText; //for name or rank
 	int amt = 5;
 	void Start () {
-		//FOR TESTING REMOVE LATER
+		//FOR TESTING REMOVE LAT
+        /*
 		PlayerPrefs.SetString("LB1", "1st,AAA,9189");
         PlayerPrefs.SetString("LB2", "2nd,BAM,8734");
         PlayerPrefs.SetString("LB3", "3rd,FFD,5935");
         PlayerPrefs.SetString("LB4", "4th,CGD,4189");
         PlayerPrefs.SetString("LB5", "5th,---,----");
-
+        */
 		//add a system to check amount of player prefs available 
 		StartCoroutine(displayValues());
     }
 
+    void Update()
+    {
+        //exit this scene script goes here
+    }
 	IEnumerator displayValues()
 	{
 		int currentLevel = 120; // y value of the text
-		yield return new WaitForSeconds(0f);
+		yield return new WaitForSeconds(0.1f);
 		for(int i = 1; i <= amt; i++)
 		{
+            
 			string key = "LB" + i;
-			string[] values = PlayerPrefs.GetString(key).Split(',');
-
+			string[] values = PlayerPrefs.GetString(key).ToString().Split(',');
+            print(PlayerPrefs.GetString(key));
 			//rank object
 			Text rank = Instantiate(smallText);
 			//set as child of the canvas
 			rank.transform.SetParent(GameObject.Find("Canvas").transform);
-            rank.text = values[0];
-			//palce it
-			rank.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
+            if (i == 1)
+            {
+                rank.text = "1st";
+            }
+            else if (i == 2)
+            {
+                rank.text = "2nd";
+            }
+            else if (i == 3)
+            {
+                rank.text = "3rd";
+            }
+            else if (i == 4)
+
+            {
+                rank.text = "4th";
+            }
+            else if (i == 5)
+            {
+                rank.text = "5th";
+            }
+            //palce it
+            rank.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
             rank.GetComponent<RectTransform>().localPosition = new Vector2(-500f, currentLevel);
 
             //name object
             Text name = Instantiate(smallText);
             //set as child of the canvas
             name.transform.SetParent(GameObject.Find("Canvas").transform);
-            name.text = values[1];
+            name.text = values[0].ToString();
             //palce it
             name.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
             name.GetComponent<RectTransform>().localPosition = new Vector2(0f, currentLevel);
@@ -59,11 +85,18 @@ public class DisplayLEaderBoard : MonoBehaviour {
             Text score = Instantiate(bigText);
             //set as child of the canvas
             score.transform.SetParent(GameObject.Find("Canvas").transform);
-            score.text = values[2];
+            score.text = values[1].ToString();
             //palce it
             score.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
             score.GetComponent<RectTransform>().localPosition = new Vector2(540.75f, currentLevel);
             currentLevel -= 120;
+            if(i == currentPlayer)
+            {
+                rank.color = Color.yellow;
+                name.color = Color.yellow;
+                score.color = Color.yellow;
+            }
+            yield return new WaitForSeconds(1f);
         }
 	}
 }
--- Intermediate Platform/Assets/Scripts/LeaderboardSetting.cs
+++ Intermediate Platform/Assets/Scripts/LeaderboardSetting.cs
@@ -4,15 +4,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class LeaderboardSetting : MonoBehaviour {
 	//this script will check if either of these players are eligible for a leaderboard position
 	//if so it will allow them to enter their name and become #cool
 	int pos = 0; char[] Playername = {'A','A','A'}; bool done = false;
 	int newHigh; string playerCode;
-	[SerializeField] Text nameInput;
+	[SerializeField] Text nameInput; int level;
 	bool player; //p1 is false, p2 is true
 	void Start () {
+        ScoreManager.score1 = 900;
+        //PlayerPrefs.DeleteAll();
+        StartCoroutine(flash());
 		if(ScoreManager.score1 > ScoreManager.score2)
 		{
 			newHigh = ScoreManager.score1;
@@ -28,56 +31,56 @@ public class LeaderboardSetting : MonoBehaviour {
 		for (int i = 1; i <= 5; i++)
 		{
             string key = "LB" + i;
-			if (PlayerPrefs.GetString(key) == null)
+            print(PlayerPrefs.GetString(key));
+			if (PlayerPrefs.GetString(key) == "")
 			{
 				//set value defaults
 				if (i == 1)
 				{
-					PlayerPrefs.SetString(key, "1st,XXX,000");
+					PlayerPrefs.SetString(key, "XXX,500");
 				}
 				else if (i == 2)
 				{
-                    PlayerPrefs.SetString(key, "2nd,XXX,000");
+                    PlayerPrefs.SetString(key, "YYY,400");
                 }
 				else if(i == 3)
 				{
-                    PlayerPrefs.SetString(key, "3rd,XXX,000");
+                    PlayerPrefs.SetString(key, "ZZZ,300");
                 }	
 				else if(i == 4)
 
                 {
-                    PlayerPrefs.SetString(key, "4th,XXX,000");
+                    PlayerPrefs.SetString(key, "ABC,200");
                 }
 				else if(i == 5)
 				{
-                    PlayerPrefs.SetString(key, "5th,XXX,000");
+                    PlayerPrefs.SetString(key, "POO,100");
                 }
 			}
-                string[] values = PlayerPrefs.GetString(key).Split(',');
-			//check if this score is greater than that one
-			if (newHigh > Convert.ToInt32(values[2]))
-			{
-				StartCoroutine(newHIGHSCORE(i));
-				return;
 
-            }
         }
+
+        for (int i = 1; i <= 5; i++)
+            {
+            string key = "LB" + i;
+            string[] values = PlayerPrefs.GetString(key).Split(',');
+            //check if this score is greater than that one
+            if (newHigh > Convert.ToInt32(values[1]))
+            {
+
+                level = i;
+                return;
+            }
+
+        }
+
+        //if this point is hit it isnt a new highscore...
+        //load the leaderboard screen after ssetting player to 0
+        DisplayLEaderBoard.currentPlayer = 0;
+        SceneManager.LoadScene(5);
         }
 	
-	IEnumerator newHIGHSCORE(int level)
-	{
-		yield return new WaitForEndOfFrame();
-		//prompt for name...
-
-		//wait until name is 3 chars long
-
-		//reorder the leaderboard accordingly
-
-		//set player val for the leaderboard display
-		//note TODO: implement that value to make that player yellow
-
-		//goto the leaderboard
-	}
+	
 	// Update is called once per frame
 	void Update () {
 		//p1
@@ -89,32 +92,116 @@ public class LeaderboardSetting : MonoBehaviour {
 		{
             playerCode = "P2";
         }
-
-        //on press
-        if (Input.GetButtonDown("Vertical" + playerCode))
+        if (pos < 3)
         {
-			//A = 65 Z = 90
-            //down
-            if ((Input.GetAxis("Vertical" + playerCode)) < 0)
+            //on press
+            if (Input.GetButtonDown("Vertical" + playerCode))
             {
-				//char = -1 ie e > d 
-				if (Playername[pos] != 65)
-				{
-					Playername[pos] =  (char)(Playername[pos] - 1);
-				}
-				else { Playername[pos] = 'Z'; }
-            }
-            //up
-            else if ((Input.GetAxis("Vertical" + playerCode)) > 0)
-            {
-                //char =1 ie a > b
-                if (Playername[pos] != 90)
+                //A = 65 Z = 90
+                //down
+                if ((Input.GetAxis("Vertical" + playerCode)) < 0)
                 {
-                    Playername[pos] = (char)(Playername[pos] + 1);
+                    //char = -1 ie e > d 
+                    if (Playername[pos] != 65)
+                    {
+                        Playername[pos] = (char)(Playername[pos] - 1);
+                    }
+                    else { Playername[pos] = 'Z'; }
                 }
-                else { Playername[pos] = 'A'; }
+                //up
+                else if ((Input.GetAxis("Vertical" + playerCode)) > 0)
+                {
+                    //char =1 ie a > b
+                    if (Playername[pos] != 90)
+                    {
+                        Playername[pos] = (char)(Playername[pos] + 1);
+                    }
+                    else { Playername[pos] = 'A'; }
 
+                }
             }
         }
+        if(Input.GetButtonDown("Fire1" + playerCode))
+        {
+            pos++;
+        }
+    }
+
+	IEnumerator flash()
+	{
+		bool a = false, b = false, c = false;
+        yield return new WaitForSeconds(0.1f);
+        //this coroutine shows the active character
+        while (pos == 0)
+		{
+            nameInput.text = Playername[0] + " " + Playername[1] + " " + Playername[2];
+			if(Playername[pos] == 'A' && !a)
+			{
+                yield return new WaitForSeconds(0.2f);
+                nameInput.text = " " + " " + Playername[1] + " " + Playername[2];
+                //yield return new WaitForSeconds(0.5f);
+            }
+			else
+			{
+				a = true;
+			}
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        while (pos == 1)
+        {
+            nameInput.text = Playername[0] + " " + Playername[1] + " " + Playername[2];
+            if (Playername[pos] == 'A' && !b)
+            {
+                yield return new WaitForSeconds(0.2f);
+                nameInput.text = Playername[0] + " " +" " + " " + Playername[2];
+                //yield return new WaitForSeconds(0.5f);
+            }
+            else
+            {
+                b = true;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        while (pos == 2)
+        {
+            nameInput.text = Playername[0] + " " + Playername[1] + " " + Playername[2];
+            if (Playername[pos] == 'A' && !c)
+            {
+                yield return new WaitForSeconds(0.2f);
+                nameInput.text = Playername[0] + " " + Playername[1] + " " + " ";
+                //yield return new WaitForSeconds(0.5f);
+            }
+            else
+            {
+                c = true;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        string playya = new string(Playername);
+        //now name is filled!
+        //reorder the leaderboard accordingly
+        List<String> players = new List<string>();
+        for(int i = 1; i <= 5; i++)
+        {
+            string key = "LB" + i;
+            players.Add(PlayerPrefs.GetString(key));
+        }
+        string input = playya + "," + newHigh;
+        players.Insert((level - 1), input.ToString());
+        //reassign playerprefs
+        for (int i = 1; i <= 5; i++)
+        {
+            string key = "LB" + i;
+            PlayerPrefs.SetString(key, players[i-1]);
+        }
+
+        //set player val for the leaderboard display
+        DisplayLEaderBoard.currentPlayer = level;
+
+        //goto the leaderboard
+        SceneManager.LoadScene(5);
     }
 }
+
